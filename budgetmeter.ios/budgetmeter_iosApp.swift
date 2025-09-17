563bc41831084c07432099ed623a40ca51f37e0a
CanSoganci <umrsgnc@gmail.com>
2025-09-17 18:46:13 +0300

    Initial Commit

---
+++ budgetmeter.ios/budgetmeter_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  budgetmeter_iosApp.swift
+//  budgetmeter.ios
+//
+//  Created by Can Soğancı on 17.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct budgetmeter_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
